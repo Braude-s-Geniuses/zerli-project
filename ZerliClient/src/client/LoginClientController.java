631b31091de138a21e9f5c8c7b78cb7099aba3bc
Marom Blumenfeld <maromblu@gmail.com>
2022-04-29 22:16:08 +0300

    Documentation

--- ZerliClient/src/client/LoginClientController.java
+++ ZerliClient/src/client/LoginClientController.java
@@ -7,12 +7,27 @@ import java.io.IOException;
 
 import static communication.MessageFromClient.LOGIN_REQUEST;
 
+/**
+ * The role of this class is to present to provide a secure connectivity to the app.
+ * By filtering, associating and documenting customers entry.
+ */
+
 public class LoginClientController {
     ZerliClient client;
 
     public LoginClientController(){
         client = Client.clientController.getClient();
     }
+
+    /**
+     * This function getting username and password from the client and check if it exists in DB,
+     * if exists provides a suitable selection menu,
+     * otherwise prints an error message on the screen.
+     *
+     * @param username, username from the client.
+     * @param password, password from the client.
+     * @return user-appropriate permission.
+     */
 
     public User tryToLogin(String username,String password){
         User newUser = new User(0,username,password,null,false,null,null,null,null,null);
--- ZerliCommon/src/user/UserType.java
+++ ZerliCommon/src/user/UserType.java
@@ -2,13 +2,17 @@ package user;
 
 import java.io.Serializable;
 
+/**
+ *  Type of users in the application.
+ */
 public enum UserType implements Serializable {
+    UNREGISTERED,
     CUSTOMER,
     BRANCH_EMPLOYEE,
     BRANCH_MANAGER,
     SERVICE_EMPLOYEE,
     EXPERT_SERVICE_EMPLOYEE,
     DELIVERY_OPERATOR,
-    CEO,
-    UNREGISTERED
+    CEO
+
 }
