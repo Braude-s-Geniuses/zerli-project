5d7aca3bb52a63cee647c9a5633fce0e98388b3c
Itshak Efraimov <itzhakefraimov@gmail.com>
2022-06-06 18:55:26 +0300

    Minor fix

---
+++ ZerliCommon/src/branch/Branch.java
@@ -0,0 +1,43 @@
+package branch;
+
+import java.io.Serializable;
+
+/**
+ * This class is used to describe a branch in the system
+ */
+public class Branch implements Serializable {
+
+    /**
+     * The branch name
+     */
+    private String branch;
+
+    public Branch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    @Override
+    public String toString() {
+        return "Branch{" +
+                "branch='" + branch + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Branch branch1 = (Branch) o;
+
+        return getBranch().equals(branch1.getBranch());
+    }
+}
